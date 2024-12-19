9352a2c7af093bcff26134a81027a54c40d81783
Madison Evans <madison.evans@postfit.co>
2024-12-18 20:14:54 -0600

    generated component pane added

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -2,7 +2,7 @@
 const MainPage = () => {
 	return (
 		<div className='bg-gray-600 w-screen h-screen overscroll-y-contain flex'>
-			<div className='bg-green-300 grow flex items-center justify-center'>component goes here</div>
+			<GeneratedComponentPane/>
 			<div className='bg-blue-500 w-1/4'>asdf</div>
 		</div>
 	);
@@ -11,3 +11,7 @@ const MainPage = () => {
 
 
 export default MainPage
+
+const GeneratedComponentPane = () => {
+    return <div className='bg-green-300 grow flex items-center justify-center'>component goes here</div>;
+}
